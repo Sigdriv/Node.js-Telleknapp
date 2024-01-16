8d04e30fd47b4da05844b07d3583506557593209
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-01-16 13:57:22 +0100

    Refactor init.sql table creation and data insertion

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE Telleknapp;
 USE Telleknapp;
 
-    DROP TABLE Telleknapp;
+DROP TABLE Telleknapp;
 
-    CREATE TABLE Telleknapp (
-        id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        count INT NOT NULL,
-        PRIMARY KEY (id)
-    );
+CREATE TABLE Telleknapp (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    count INT NOT NULL,
+    PRIMARY KEY (id)
+);
 
-    INSERT INTO Telleknapp (name, count) VALUES ('Telleknapp-1', 0), ('Telleknapp-2', 0), ('Telleknapp-3', 0), ('Telleknapp-4', 0), ('Telleknapp-5', 0);
+INSERT INTO Telleknapp (name, count) VALUES ('Telleknapp-1', 0), ('Telleknapp-2', 0);
 
 -- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'passord';
